fb8a75a8b1a48a2a5d898166bb1ca17b1d8d32ef
Камилла Хусаинова <kamillahusainova957@gmail.com>
2026-05-31 21:24:26 +0300

    "в stats был создан класс ReportGenerator, функция __init__, функция add, функция generate, добавлен блок обработки ошибок в handler

--- system/handler.py
+++ system/handler.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import logging
+from stats import ReportGenerator
 
 class MailHandler:
     def __init__(self, inbox_path, processed_path):
@@ -13,33 +14,49 @@ class MailHandler:
         format="%(asctime)s - %(message)s")
 
     def handle_all(self):
+        stats = ReportGenerator()
         for filename in os.listdir(self.inbox_path):
             filepath = os.path.join(self.inbox_path, filename)
-            self.handle_one(filepath)
+            if os.path.isfile(filename):
+                self.handle_one(filepath)
+        stats.generate()
+        return stats
 
     def handle_one(self, filepath):
-        with open(filepath, "r", encoding="utf-8") as source:
-            raw_content = source.read()
+        try:
+            with open(filepath, "r", encoding="utf-8") as source:
+                raw_content = source.read()
 
-        name = ""
-        theme = ""
-        content = ""
-        for line in raw_content.split("\n"):
-            if line.startswith("От кого:"):
-                name = line.replace("От кого:", "").strip()
-            elif line.startswith("Тема:"):
-                theme = line.replace("Тема:", "").strip()
-        
-        parts = raw_content.split("\n\n", 1)
-        if len(parts) > 1:
-            content = parts[1].strip()
+            name = ""
+            theme = ""
+            content = ""
+            for line in raw_content.split("\n"):
+                if line.startswith("От кого:"):
+                    name = line.replace("От кого:", "").strip()
+                elif line.startswith("Тема:"):
+                    theme = line.replace("Тема:", "").strip()
+            
+            parts = raw_content.split("\n\n", 1)
+            if len(parts) > 1:
+                content = parts[1].strip()
 
-        email = Email(name=name, content=content, theme=theme, path=filepath)
-        category = self.classifier.classify(email)
+            email = Email(name=name, content=content, theme=theme, path=filepath)
+            category = self.classifier.classify(email)
 
-        destination = os.path.join(self.processed_path, category)
-        shutil.move(filepath, destination)
-        logging.info(str(email) + f"Категория: {category}")
+            destination = os.path.join(self.processed_path, category)
+            shutil.move(filepath, destination)
+            logging.info(str(email) + f"Категория: {category}")
+
+        except UnicodeDecodeError:
+            logging.warning(f'Не удалось прочитать файл: {filepath}')
+            shutil.move(filepath, os.path.join(self.processed_path, 'unknown'))
+            return 'unknown', False
+        except FileNotFoundError:
+            logging.error(f'Файл не найден: {filepath}')
+            return None
+        except Exception as e:
+            logging.warning(f'Ошибка обработки {filepath}: {e}')
+            return 'unknown', False
 
 
 
---
+++ system/stats.py
@@ -0,0 +1,26 @@
+class ReportGenerator:
+    def __init__(self):
+        self.stats = {}
+
+    def add(self, category, is_urgent):
+        if category not in self.stats:
+            self.stats[category] = {'total': 0, 'urgent': 0}
+        self.stats[category]['total'] += 1
+        if is_urgent:
+            self.stats[category]['urgent'] += 1
+
+    def generate(self):
+        summ_letters = sum(data['total'] for data in self.stats.values())
+        report_lines = []
+        report_lines.append('Отчёт обработки почты')
+        for category, data in self.stats.items():
+            if category == 'unknown':
+                report_lines.append(f'{category}: {data['total']} письмо')
+            else:
+                report_lines.append(f'{category}: {data['total']} писем, {data['urgent']} важных')
+        report_lines.append(f'Итого: {summ_letters} писем')
+        for line in report_lines:
+            print(line)
+        with open('stats.txt', 'w', encoding = 'utf-8') as f:
+            for line in report_lines:
+                f.write(line + '\n')
