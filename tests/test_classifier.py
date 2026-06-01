8899fa7fc0a637bf1580d657c16789d5b9266934
Шакирова Лилия Денисовна <lideshakirova@edu.hse.ru>
2026-06-01 11:19:47 +0300

    добавлен новый ключ для Classifier

--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -2,3 +2,13 @@ import pytest
 from system.classifier import Classifier
 from system.mail import Email
 
+def test_spam(classifier, spam_email):
+    assert classifier.classify(spam_email) == "spam"
+
+def test_urgent(classifier, urgent_email):
+    res = classifier.classify(urgent_email)
+    assert res == 'security' 
+    assert urgent_email.is_urgent == True
+
+def test_monitoring(classifier, monitoring_email):
+    assert classifier.classify(monitoring_email) == "monitoring"
